8641d60a476ca2d4b4b248fda9c94cb17e4aef41
maxilukowski <maxi.lukowski@yahoo.com>
2020-10-29 16:11:20 +0100

    Refactor ListNameEdit0r

--- src/components/ShoppingList/DropDownListNames.js
+++ src/components/ShoppingList/DropDownListNames.js
@@ -1,25 +1,40 @@
 import React from 'react'
 import styled from 'styled-components'
+import Button from '../Buttons'
 
 const DropDownListNames = ({
   listNames,
   setShoppingListName,
   shoppingListName,
+  setToggle,
+  setListToRemove,
+  toggle,
+  setListNames,
 }) => {
   return (
-    <StyledSelect
-      value={shoppingListName}
-      onChange={(event) => setShoppingListName(event.target.value)}
-      data-testid="dropdown-select-listnames"
-    >
-      {listNames.map((listName, index) => (
-        <option key={index} data-testid="dropdown-option-listnames">
-          {listName}
-        </option>
-      ))}
-      {/* useRef für initial */}
-    </StyledSelect>
+    <Spacer>
+      <StyledSelect
+        value={shoppingListName}
+        onChange={(event) => setShoppingListName(event.target.value)}
+        data-testid="dropdown-select-listnames"
+      >
+        {listNames.map((listName, index) => (
+          <option key={index} data-testid="dropdown-option-listnames">
+            {listName}
+          </option>
+        ))}
+      </StyledSelect>
+      <Button fontSize="20px" text="del" mr="15px" onClick={removeListName} />
+      <Button fontSize="20px" text="new" onClick={() => setToggle(!toggle)} />
+    </Spacer>
   )
+  function removeListName() {
+    const newListNames = listNames.filter((listName) => {
+      if (listName === shoppingListName) setListToRemove(listName)
+      return listName !== shoppingListName
+    })
+    setListNames(newListNames)
+  }
 }
 
 export default DropDownListNames
@@ -32,4 +47,10 @@ const StyledSelect = styled.select`
   outline: none;
   font-size: 20px;
   background: var(--sand);
+`
+
+const Spacer = styled.div`
+  margin-bottom: 10px;
+  display: flex;
+  justify-content: space-between;
 `
