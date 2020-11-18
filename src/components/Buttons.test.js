883e7729808d42fc6a350c8fb2329c056877e5c1
maxilukowski <maxi.lukowski@yahoo.com>
2020-11-18 14:09:46 +0100

    Updated button.test.js

--- src/components/Buttons.test.js
+++ src/components/Buttons.test.js
@@ -1,20 +1,42 @@
 import React from 'react'
-import Button from './Buttons'
+import {
+  DeleteListNameButton,
+  AddListNameButton,
+  DeleteListItemsButton,
+} from './Buttons'
 import { fireEvent, render, screen } from '@testing-library/react'
 import '@testing-library/jest-dom/extend-expect'
 
-describe('Button.test.js', () => {
+describe('Buttons.test.js', () => {
   let testFunction
-  const text1 = 'foo'
+  const dataTestId = 'testId'
+  const dataTestId2 = 'testId2'
+  const dataTestId3 = 'testId3'
   beforeEach(() => {
     testFunction = jest.fn()
-    render(<Button onClick={testFunction} text={text1} />)
+    render(
+      <>
+        <DeleteListNameButton onClick={testFunction} dataTestid={dataTestId} />
+        <AddListNameButton onClick={testFunction} dataTestid={dataTestId2} />
+        <DeleteListItemsButton
+          onClick={testFunction}
+          dataTestid={dataTestId3}
+        />
+      </>
+    )
+    screen.debug()
   })
-  it('renders a child element', () => {
-    expect(screen.getByText(text1)).toBeInTheDocument()
+  it('gets rendered', () => {
+    expect(screen.getByTestId(dataTestId)).toBeInTheDocument()
+    expect(screen.getByTestId(dataTestId2)).toBeInTheDocument()
+    expect(screen.getByTestId(dataTestId3)).toBeInTheDocument()
   })
   it('reacts on a click event', () => {
-    fireEvent.click(screen.getByText(text1))
+    fireEvent.click(screen.getByTestId(dataTestId))
     expect(testFunction).toHaveBeenCalledTimes(1)
+    fireEvent.click(screen.getByTestId(dataTestId2))
+    expect(testFunction).toHaveBeenCalledTimes(2)
+    fireEvent.click(screen.getByTestId(dataTestId3))
+    expect(testFunction).toHaveBeenCalledTimes(3)
   })
 })
